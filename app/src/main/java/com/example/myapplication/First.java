9be4379c4fb5452f2c5d6c8c49a10ee5d2372912
ntaox <youiserror@gmail.com>
2020-12-19 13:48:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/First.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+public class First extends Fragment{
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.first,container,false);
+
+    }
+
+}
